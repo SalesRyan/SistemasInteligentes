97a5f208395ff832874fa94740d64a45cf7456cb
VitoriaCarvalho <vitoriiacb@gmail.com>
2019-10-01 20:23:32 -0300

    Plot com a variação dos sigmas concluído, porém, tem um bug.

--- Prática 2 - parte 1/main_part1.m
+++ Prática 2 - parte 1/main_part1.m
@@ -19,7 +19,7 @@ model2 = svmTrain(X, y, 10, @linearKernel, 1e-3, 10);
 model3 = svmTrain(X, y, 100, @linearKernel, 1e-3, 10);
 model4 = svmTrain(X, y, 1000, @linearKernel, 1e-3, 10);
 
-models = [model1, model2, model3, model4]
+models = [model1, model2, model3, model4];
 
 for i = 1:4
   w = models(i).w;
@@ -28,12 +28,12 @@ for i = 1:4
   yp = - (w(1)*xp + b)/w(2);
   hold on;
   plot(xp, yp); 
-  %hold off
-  print -dpng 'fronteira_decisao_C1.png' -S640,640
+  hold off
+  %print -dpng 'fronteira_decisao_C1.png' -S640,640
 end
 
 legend({'C = 1', 'C = 10', 'C = 100', 'C = 1000'});
-title('Fronteira de decisão linear')
+title('Fronteira de decisão linear');
 hold on
 plotData(X, y);
 
@@ -64,6 +64,36 @@ load('ex6data2.mat');
 % SVM Parameters
 C = 1; sigma = 0.1;
 
-model= svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma));
+%model= svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma));
+kernel1 = svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, 0.01));
+kernel2 = svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, 0.03));
+kernel3 = svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, 0.1));
+kernel4 = svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, 0.3));
+kernel5 = svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, 1));
+
+kernels = [kernel1, kernel2, kernel3, kernel4, kernel5];
+colors = ['b', 'g', 'r', 'y', 'k'];
+
+figure
+for k = 1:5
+  x1plot = linspace(min(X(:,1)), max(X(:,1)), 100)';
+  x2plot = linspace(min(X(:,2)), max(X(:,2)), 100)';
+  [X1, X2] = meshgrid(x1plot, x2plot);
+  vals = zeros(size(X1));
+  for i = 1:size(X1, 2)
+     this_X = [X1(:, i), X2(:, i)];
+     vals(:, i) = svmPredict(kernels(k), this_X);
+  end
+  % Plota a frontreira aprendida pelo SVM
+  hold on
+  contour(X1, X2, vals, [0.5 0.5], colors(k));
+  %print -dpng 'fronteira_decisao_nlinear_C1_sigma01.png' -S640,640
+end
+
+legend({'Sigma = 0.01', 'Sigma = 0.03', 'Sigma = 0.1', 'Sigma = 0.3', 'Sigma = 1'});
+title('Fronteira de decisão não-linear');
+hold on
+plotData(X, y);
+
 %visualizeBoundary(X, y, model);
 
